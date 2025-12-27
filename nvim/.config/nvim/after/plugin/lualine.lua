2d3286fcdc650e3ce925947c9b7178b08e812a9a
Stiffpixels <saifimuzammil2@gmail.com>
2025-12-27 17:52:14 +0530

    removed encoding from lualine section x

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -1,13 +1,30 @@
-require('lualine').setup({
+require('lualine').setup {
     options = {
         theme = 'gruvbox',
-        sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch', 'diff', 'diagnostics' },
-            lualine_c = { 'filename' },
-            -- lualine_x = { 'encoding', 'fileformat', 'filetype' },
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' }
-        },
-    }
-})
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
+    -- inactive_sections = {
+    --     lualine_a = {},
+    --     lualine_b = {},
+    --     lualine_c = {},
+    --     lualine_x = { 'encoding' },
+    --     lualine_y = {},
+    --     lualine_z = {}
+    -- },
+}
+
+--         sections = {
+--             lualine_a = { 'mode' },
+--             lualine_b = { 'branch', 'diff', 'diagnostics' },
+--             lualine_c = { 'filename' },
+-- lualine_x = { 'encoding', 'fileformat', 'filetype' },
+--             lualine_y = { 'progress' },
+--             lualine_z = { 'location' }
+--         },
