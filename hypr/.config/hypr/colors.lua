09fbf7133dfb4cfe0e9b7cc9eddbb4d40e45adcb
Stiffpixels <saifimuzammil2@gmail.com>
2026-07-19 14:48:28 +0530

    fixed hyprland config

---
+++ hypr/.config/hypr/colors.lua
@@ -0,0 +1,55 @@
+-- Material Design 3 Palette for Neovim / Lua Configurations
+-- Standardized with standard #RRGGBB format for GUI elements
+
+return {
+  background = "#1a110f", -- Full raw: 1a110fff
+  error = "#ffb4ab", -- Full raw: ffb4abff
+  error_container = "#93000a", -- Full raw: 93000aff
+  inverse_on_surface = "#392e2b", -- Full raw: 392e2bff
+  inverse_primary = "#8f4c35", -- Full raw: 8f4c35ff
+  inverse_surface = "#f1dfda", -- Full raw: f1dfdaff
+  on_background = "#f1dfda", -- Full raw: f1dfdaff
+  on_error = "#690005", -- Full raw: 690005ff
+  on_error_container = "#ffdad6", -- Full raw: fdad6ff
+  on_primary = "#0C354D", -- Full raw: 0C354Dff
+  on_primary_container = "#ffdbd0", -- Full raw: ffdbd0ff
+  on_primary_fixed = "#390c00", -- Full raw: 390c00ff
+  on_primary_fixed_variant = "#723520", -- Full raw: 723520ff
+  on_secondary = "#442a21", -- Full raw: 442a21ff
+  on_secondary_container = "#ffdbd0", -- Full raw: ffdbd0ff
+  on_secondary_fixed = "#2c160e", -- Full raw: 2c160eff
+  on_secondary_fixed_variant = "#5d4036", -- Full raw: 5d4036ff
+  on_surface = "#f1dfda", -- Full raw: f1dfdaff
+  on_surface_variant = "#d8c2bb", -- Full raw: d8c2bbff
+  on_tertiary = "#3a3005", -- Full raw: 3a3005ff
+  on_tertiary_container = "#f3e2a7", -- Full raw: f3e2a7ff
+  on_tertiary_fixed = "#221b00", -- Full raw: 221b00ff
+  on_tertiary_fixed_variant = "#51461a", -- Full raw: 51461aff
+  outline = "#a08d87", -- Full raw: a08d87ff
+  outline_variant = "#53433f", -- Full raw: 53433fff
+  primary = "#91D7FF", -- Full raw: 91D7FFff
+  primary_container = "#723520", -- Full raw: 723520ff
+  primary_fixed = "#ffdbd0", -- Full raw: ffdbd0ff
+  primary_fixed_dim = "#ffb59d", -- Full raw: ffb59dff
+  scrim = "#000000", -- Full raw: 000000ff
+  secondary = "#e7bdb0", -- Full raw: e7bdb0ff
+  secondary_container = "#5d4036", -- Full raw: 5d4036ff
+  secondary_fixed = "#ffdbd0", -- Full raw: ffdbd0ff
+  secondary_fixed_dim = "#e7bdb0", -- Full raw: e7bdb0ff
+  shadow = "#000000", -- Full raw: 000000ff
+  source_color = "#f96732", -- Full raw: f96732ff
+  surface = "#1a110f", -- Full raw: 1a110fff
+  surface_bright = "#423733", -- Full raw: 423733ff
+  surface_container = "#271d1b", -- Full raw: 271d1bff
+  surface_container_high = "#322825", -- Full raw: 322825ff
+  surface_container_highest = "#3d322f", -- Full raw: 3d322fff
+  surface_container_low = "#231917", -- Full raw: 231917ff
+  surface_container_lowest = "#140c0a", -- Full raw: 140c0aff
+  surface_dim = "#1a110f", -- Full raw: 1a110fff
+  surface_tint = "#ffb59d", -- Full raw: ffb59dff
+  surface_variant = "#53433f", -- Full raw: 53433fff
+  tertiary = "#d7c68d", -- Full raw: d7c68dff
+  tertiary_container = "#51461a", -- Full raw: 51461aff
+  tertiary_fixed = "#f3e2a7", -- Full raw: f3e2a7ff
+  tertiary_fixed_dim = "#d7c68d", -- Full raw: d7c68dff
+}
